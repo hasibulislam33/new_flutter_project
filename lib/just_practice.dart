6ad93beed68a4a556db86ad8b59f5b1d53879180
hasibul <hasibul9321@gmail.com>
2025-09-12 10:09:26 +0530

    Finioshed second class

--- lib/just_practice.dart
+++ lib/just_practice.dart
@@ -5,45 +5,115 @@ class Practice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController phonenumber = TextEditingController();
+    TextEditingController password = TextEditingController();
     return Scaffold(
+      backgroundColor: Colors.blue[40],
       appBar: AppBar(
-        backgroundColor: Colors.grey,
-        centerTitle: true,
-        title: Text("Practice",
-        style: TextStyle(
-          fontSize: 20,
-          shadows: [
-            Shadow(
-              offset: Offset(3, 3),
-              blurRadius: 5,
-              color: Colors.black26
-            )
-          ],
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),)
+        backgroundColor: Colors.blue,
+        title: Center(
+          child: Text("Practice",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                offset: Offset(3, 3),
+                blurRadius: 5,
+                color: Colors.black26
+              )
+            ]
+          ),),
+        ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: SizedBox(width: 200,height: 30,
-              child: ElevatedButton(style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red[500],
-                foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: phonenumber,
+              keyboardType: TextInputType.phone,
+              maxLength: 11,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.blue[60],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.blue, width: 2
+                  )
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.red,width: 3)
+                ),
+                labelText: "number",
+                labelStyle:TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold
+                ),
+
+                hintText: "Enter Phone number",
+                hintStyle: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
                 )
               ),
-                  onPressed: (){}, child: Text("Sumit")),
             ),
-          ),
-          SizedBox(height: 30,),
-          Center(child: GestureDetector(
-              onTap: (){
-                print("User traped");
-          },child: Text("Clikable Text")))
-        ],
+            TextField(
+              obscureText: true,
+              controller: password,
+              decoration: InputDecoration(
+                filled: true,
+                  fillColor: Colors.blue[60],
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                          color: Colors.blue, width: 2
+                      )
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.red,width: 3)
+                  ),
+                  labelText: "Password",
+                  labelStyle:TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold
+                  ),
+
+                  hintText: "Enter Phone Password",
+                  hintStyle: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+            ),
+            SizedBox(height: 30,),
+            SizedBox(width: 200,height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+                  onPressed: (){
+                  print(phonenumber.text);
+                  print(password.text);
+                  phonenumber.clear();
+                  password.clear();
+                  }, child: Text("Login",
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),)),
+            ),
+            SizedBox(height: 30,),
+            InkWell( onTap: (){
+              print("User taped");
+            },
+                child: Text("This is Gesture text"))
+          ],
+        ),
       ),
     );
   }
